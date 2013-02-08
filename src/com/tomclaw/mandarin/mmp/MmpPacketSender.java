e109f40bf0f17832ea2e0f14609ef4ebab34020b
solkin <inbox@tomclaw.com>
2013-02-08 12:03:37 +0400

    Updated libs, partial encoding fixes

--- src/com/tomclaw/mandarin/mmp/MmpPacketSender.java
+++ src/com/tomclaw/mandarin/mmp/MmpPacketSender.java
@@ -41,12 +41,12 @@ public class MmpPacketSender {
     packet.data.append( DataUtil.mmputil_prepareByteStringWthLength(
             MmpStatusUtil.getStatusName( statusId ) ) );
     packet.data.append( DataUtil.mmputil_prepareBytesWthLength(
-            StringUtil.stringToByteArray1251(
+            StringUtil.string1251ToByteArray(
             Localization.getMessage( MmpStatusUtil.getStatusDescr(
             MmpStatusUtil.getStatusIndex( statusId ) ) ) ) ) );
     packet.data.append( DataUtil.mmputil_prepareBytesWthLength(
-            // StringUtil.string1251ToByteArray( statusString ) ) ); // for protocol version 0x00010016
-            StringUtil.stringToByteArray1251( statusString ) ) ); // for protocol version 0x0001000e
+            StringUtil.string1251ToByteArray( statusString ) ) ); // for protocol version 0x00010016
+            // StringUtil.stringTobyteArray1251( statusString ) ) ); // for protocol version 0x0001000e
     if ( isSendClientInfo ) {
       DataUtil.put32_reversed( temp, 0, MmpSession.clientId.length()
               + MmpSession.mraVer.length() );
